16d3a6913fd913f0603bee4f35277134b3556f26
Dyglaz <kallkae@mail.ru>
2022-10-18 16:32:57 +0300

    Выполнил практическую работу 4_1

---
+++ src/ru/mirea/task4/task4_1_2/Phone.java
@@ -0,0 +1,41 @@
+package ru.mirea.task4.task4_1_2;
+
+public class Phone {
+    private String number;
+    private String model;
+    private String weight;
+
+    public Phone() {}
+    public Phone(String number, String model, String weight)
+    {
+        this.number = number;
+        this.model = model;
+        this.weight = weight;
+        Phone phone4 = new Phone("83225551488", "Alcatel 2019G");
+    }
+    public Phone(String number, String model)
+    {
+        this.number = number;
+        this.model = model;
+    }
+
+    public void receiveCall(String name) {
+        System.out.println("Calls: " + name);
+    }
+
+    public void receiveCall(String name, String number) {
+        System.out.println("Calls: " + name + " " + number);
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void sendMessage(String number1, String number2) {
+        System.out.println("The message will be sent: " + number1 + " and " + number2);
+    }
+
+    public String toString() {
+        return "number = " + number + ", model = " + model + ", weight = " + weight;
+    }
+}
---
+++ src/ru/mirea/task4/task4_1_2/TestPhone.java
@@ -0,0 +1,25 @@
+package ru.mirea.task4.task4_1_2;
+
+public class TestPhone {
+    public static void main(String [ ] args) {
+        Phone phone1 = new Phone("89150464085", "iPhone XR", "194");
+        Phone phone2 = new Phone("88005553535", "Nokia 3310", "151");
+        Phone phone3 = new Phone("89999999999", "iPhone 14", "174");
+
+        System.out.println("Instance Information: " + phone1.toString());
+        System.out.println("Instance Information: " + phone2.toString());
+        System.out.println("Instance Information: " + phone3.toString());
+
+        phone1.receiveCall("Maxim");
+        phone2.receiveCall("Gosha");
+        phone3.receiveCall("Denis");
+
+        System.out.println("Phone number: " + phone1.getNumber());
+        System.out.println("Phone number: " + phone2.getNumber());
+        System.out.println("Phone number: " + phone3.getNumber());
+
+        phone1.receiveCall("Maxim", phone1.getNumber());
+
+        phone1.sendMessage(phone2.getNumber(), phone3.getNumber());
+    }
+}
